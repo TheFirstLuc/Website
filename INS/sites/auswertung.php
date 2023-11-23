0998f4393e0e7a2f17612b0b83969d8698330419
Luc <thefirstluc@gmail.com>
2023-11-23 12:48:18 +0100

    Php done

--- INS/sites/auswertung.php
+++ INS/sites/auswertung.php
@@ -1,34 +1,65 @@
 <?php
-# check password
-//if(!($_POST["passwort"] == 'Internetsprachen'))
-//    return;
+checkPassword();
+checkForEmailError();
+checkForRueckfragenError();
+@ratingInhalt();
+@ratingAussehen();
+getDatum();
+getName();
 
-# check for email error
-if (empty($_POST['kopie']) && !empty($_POST['email'])) {
-    echo("Error: E-Mail set but checkbox isn't selected" . PHP_EOL);
-    return;
+function checkPassword(): void
+{
+    if (!($_POST["passwort"] == 'Internetsprachen')) {
+        echo("Password is incorrect");
+        exit();
+    }
 }
 
-# check for Rückfragen error
-if (!(isset($_POST['rueckfragen']) && (($_POST['email'] != "") || ($_POST['telefon'] != "") || ($_POST['website'] != "")))) {
-    echo ("<p>Error: Rückfragen selected but no data provided</p>");
-    return;
+function checkForEmailError(): void
+{
+    if (empty($_POST['kopie']) && !empty($_POST['email'])) {
+        echo("Error: E-Mail set but checkbox isn't selected" . PHP_EOL);
+        exit();
+    }
+}
+
+function checkForRueckfragenError(): void
+{
+    if (!(isset($_POST['rueckfragen']) && (($_POST['telefon'] != "") || ($_POST['website'] != "")))) {
+        echo("<p>Error: Data provided but not Rückfragen selected</p>");
+        exit();
+    }
+
+    if (empty($_POST['email']) && empty($_POST['telefon']) && empty($_POST['website'])) {
+        echo("<p>Error: Rückfragen selected but no data provided</p>");
+        exit();
+    }
 }
 
 # rating Inhalt
-$rating = $_POST['note_inhalt'];
-if($rating == 1 || $rating == 2)
-    echo "<p>Ich freue mich das Ihnen der Inhalt der gefallen hat.</p>";
-elseif ($rating == 3)
-    echo "<p>Ich werde mich bemühen in Zukunft den Inhalt der Seite zu optimieren.</p>";
-else
-    echo "<p>Tut mir leid :,(</p>";
+function ratingInhalt(): void
+{
+    $rating = $_POST['note_inhalt'];
+    if ($rating == 1 || $rating == 2)
+        echo "<p>Ich freue mich das Ihnen der Inhalt der gefallen hat.</p>";
+    elseif ($rating == 3)
+        echo "<p>Ich werde mich bemühen in Zukunft den Inhalt der Seite zu optimieren.</p>";
+    else
+        echo "<p>Tut mir leid :,(</p>";
+}
 
-# rating Aussehen
-echo ("<p>Danke dass Sie das Aussehen mit ". $_POST['note_aussehen']. " bewertet haben.</p>");
+function ratingAussehen(): void
+{
+    echo("<p>Danke dass Sie das Aussehen mit " . $_POST['note_aussehen'] . " bewertet haben.</p>");
+}
 
-# date
-echo ("<p>Ihre Feedbackwerte wurden am ".date("d.m.Y")." um ".date("H:i")." Uhr angenommen</p>");
+function getDatum(): void
+{
+    echo("<p>Ihre Feedbackwerte wurden am " . date("d.m.Y") . " um " . date("H:i") . " Uhr angenommen</p>");
+}
 
-echo("<p>". $_POST['vorname']."</p>");
+function getName(): void
+{
+    echo("<p>" . $_POST['vorname'] . "</p>");
+}
 ?>
